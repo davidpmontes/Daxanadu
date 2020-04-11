f3638add8a66be479ab560ce0f7a119d24467791
David Montes <david.montes@sjsu.edu>
2020-04-11 02:14:59 -0700

    added converstation boxes

---
+++ Assets/Scripts/Conversation.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Conversation : MonoBehaviour
+{
+    public static Conversation Instance;
+
+    [SerializeField] private GameObject portraitDialogueBox;
+    [SerializeField] private GameObject landscapeDialogueBox;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void ShowConversation()
+    {
+        portraitDialogueBox.SetActive(true);
+        landscapeDialogueBox.SetActive(true);
+    }
+
+    public void HideConversation()
+    {
+        portraitDialogueBox.SetActive(false);
+        landscapeDialogueBox.SetActive(false);
+    }
+}
