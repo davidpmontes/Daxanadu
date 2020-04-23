e5ecffe4d988f9070f6b9157f20842b150b298fa
David Montes <david.montes@sjsu.edu>
2020-04-23 01:55:40 -0700

    improved magic projectiles

--- Assets/Magic.cs
+++ Assets/Magic.cs
@@ -2,40 +2,10 @@
 
 public class Magic : MonoBehaviour
 {
-    private int direction = 1;
-    private float speed = 5;
+    [SerializeField] private GameObject fireball;
 
-    private void Awake()
+    public void OnMagicStart()
     {
-        gameObject.SetActive(false);
-    }
-
-    private void OnEnable()
-    {
-        direction = (int)Player.Instance.transform.localScale.x;
-        transform.position = Player.Instance.transform.position;
-        transform.localScale = Player.Instance.transform.localScale;
-        Invoke("DeactivateAfterTime", 3);
-    }
-
-    private void Update()
-    {
-        transform.Translate(new Vector2(direction * speed * Time.deltaTime, 0));
-    }
-
-    private void DeactivateAfterTime()
-    {
-        gameObject.SetActive(false);
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        Debug.Log("magic collide");
-        if (collision.gameObject.name == "greenSlime")
-        {
-            collision.gameObject.GetComponent<GreenSlime>().OnReceiveDamage(transform.position);
-            CancelInvoke("DeactivateAfterTime");
-            gameObject.SetActive(false);
-        }
+        Instantiate(fireball, transform);
     }
 }
---
+++ Assets/MagicProjectile.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class MagicProjectile : MonoBehaviour
+{
+    private int direction = 1;
+    private float speed = 5;
+    public float lifespan;
+
+    private void Start()
+    {
+        transform.position = Player.Instance.transform.position;
+        transform.localScale = Player.Instance.transform.localScale;
+        direction = (int)Player.Instance.transform.localScale.x;
+        lifespan = 2;
+        Debug.Log(direction);
+    }
+
+    private void Update()
+    {
+        transform.Translate(new Vector2(direction * speed * Time.deltaTime, 0));
+        lifespan -= Time.deltaTime;
+        if (lifespan <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "greenSlime")
+        {
+            collision.gameObject.GetComponent<GreenSlime>().OnReceiveDamage(transform.position);
+            Destroy(gameObject);
+        }
+    }
+}
